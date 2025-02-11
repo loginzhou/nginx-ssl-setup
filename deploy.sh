1c058e8e1dcea6b8e476fe983983c8db4502e328
loginzhou <38754178+loginzhou@users.noreply.github.com>
2025-02-11 08:58:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 安装必要的依赖
+sudo apt update
+sudo apt install -y curl nginx
+
 # 从 GitHub 下载 index.html 文件
 if ! curl -o /var/www/html/index.html https://raw.githubusercontent.com/loginzhou/nginx-ssl-setup/main/index.html; then
     echo "Failed to download index.html. Exiting..."
