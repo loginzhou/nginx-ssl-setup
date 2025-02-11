5de42ab0d2354c081d07dcc6d9b926d9a5c4414e
loginzhou <38754178+loginzhou@users.noreply.github.com>
2025-02-11 08:57:24 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,15 @@ if ! curl -o /var/www/html/index.html https://raw.githubusercontent.com/loginzho
     exit 1
 fi
 
+# 从 GitHub 下载 index.nginx-debian.html 文件
+if ! curl -o /var/www/html/index.nginx-debian.html https://raw.githubusercontent.com/loginzhou/nginx-ssl-setup/main/index.nginx-debian.html; then
+    echo "Failed to download index.nginx-debian.html. Exiting..."
+    exit 1
+fi
+
 # 修改文件权限
-sudo chown www-data:www-data /var/www/html/index.html
-sudo chmod 644 /var/www/html/index.html
+sudo chown www-data:www-data /var/www/html/index.html /var/www/html/index.nginx-debian.html
+sudo chmod 644 /var/www/html/index.html /var/www/html/index.nginx-debian.html
 
 # 重启 Nginx 使更改生效
 sudo systemctl restart nginx
